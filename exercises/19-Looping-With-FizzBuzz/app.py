74258acf8fe9ffae06643542d839d7b7809c23e3
Tibisfly <vielmatorotibisay@gmail.com>
2020-11-25 10:28:51 +0000

    finish

--- exercises/19-Looping-With-FizzBuzz/app.py
+++ exercises/19-Looping-With-FizzBuzz/app.py
@@ -1,28 +1,14 @@
 import random
 
-# def fizz_buzz():
-#     for i in range(1, 101):
-#         if (i % 3 == 0):
-#             print("Fizz")
-#         elif (i % 5 ==0):
-#             print("Buzz")
-#         elif (i % 3 == 0) and (i % 5 == 0):
-#             print("FizzBuzz")
-#         print(i)
-   
-# fizz_buzz()
-
 def fizz_buzz():
-# your code here
-    for i in range(1,101):
-        if (i % 3 == 0):
-            print ("Fizz")
-            continue
-        elif (i % 5==0):
-            print("Buzz")
-            continue
-        elif (i % 3 == 0) and (i % 5==0):
+    for i in range(1, 101):
+        if (i % 3 == 0) and (i % 5 == 0):
             print("FizzBuzz")
-        print(i)
-    
+        elif (i % 3 == 0):
+            print("Fizz")
+        elif (i % 5 ==0):
+            print("Buzz")
+        else: print(i)
+
+   
 fizz_buzz()
